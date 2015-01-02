16c2fe813f930e2c3605829bb08571142035a995
Alexis Montagne <alexis.montagne@gmail.com>
2015-01-02 11:48:35 +0100

    Add initial implementation of sensu-fleet-client

---
+++ clients/sensu-fleet-client/check-fleet-status.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'rubygems' if RUBY_VERSION < '1.9.0'
+require 'sensu-plugin/check/cli'
+
+class FleetCheck < Sensu::Plugin::Check::CLI
+  option :etcd_ip,
+         short: '-e ETCD_IP',
+         long: '--etcd_ip ETCD_IP',
+         description: 'ETCD peer ip',
+         default: 'http://172.17.42.1:4001'
+
+  def run
+    cmd = `fleetctl --endpoint #{config[:etcd_ip]} list-units -fields "sub,unit" -no-legend | grep failed`
+    failed_units = cmd.split("\n").map { |line| line.split("\t").last }
+
+    if failed_units.any?
+      critical "#{failed_units.join(',')} are stuck"
+    else
+      ok 'Everything is going well inside the cluster'
+    end
+  end
+end
