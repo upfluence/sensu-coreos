4b54d30457de5efe0aea71c8bb67927c23170e30
Alexis Montagne <alexis.montagne@gmail.com>
2015-07-03 17:29:19 -0700

    Improve the output of the fleet-check

--- clients/sensu-fleet-client/check-fleet-status.rb
+++ clients/sensu-fleet-client/check-fleet-status.rb
@@ -12,12 +12,23 @@ class FleetCheck < Sensu::Plugin::Check::CLI
 
   def run
     cmd = `fleetctl --endpoint #{config[:etcd_ip]} list-units -fields "sub,unit" -no-legend | grep failed`
+
     failed_units = cmd.split("\n").map { |line| line.split("\t").last }
 
     if failed_units.any?
-      critical "#{failed_units.join(',')} are stuck"
-    else
-      ok 'Everything is going well inside the cluster'
+      critical "Failed units: #{failed_units.join(',')}"
     end
+
+    cmd = `fleetctl --endpoint #{config[:etcd_ip]} list-unit-files -fields="unit,dstate,state" -no-legend`
+
+    warning_units = cmd.split("\n").map { |l| l.split("\t") }.reject do |l|
+      l.last == "-"
+    end.select { |l| l[-2] != l[-1] }.map(&:first)
+
+    if warning_units.any?
+      warning "Units in a wrong state: #{warning_units.join(',')}"
+    end
+
+    ok 'Everything is going well inside the cluster'
   end
 end
