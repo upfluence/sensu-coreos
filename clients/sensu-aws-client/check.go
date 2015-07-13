f964b6e43b2516ff2b8f8d426f175c34338594f6
Alexis Montagne <alexis.montagne@gmail.com>
2015-07-13 16:41:23 -0700

    Check etcd instances on each machine of the cluster

--- clients/sensu-aws-client/check.go
+++ clients/sensu-aws-client/check.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/mitchellh/goamz/aws"
 	"github.com/mitchellh/goamz/ec2"
@@ -14,14 +17,89 @@ import (
 	"github.com/upfluence/sensu-client-go/sensu/transport"
 )
 
-func AWSCheck() check.ExtensionCheckResult {
+func buildEc2Client() *ec2.EC2 {
 	auth := aws.Auth{os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), ""}
-	client := ec2.New(auth, aws.USEast)
+	return ec2.New(auth, aws.USEast)
+}
+
+func EtcdGlobalCheck() check.ExtensionCheckResult {
+	client := buildEc2Client()
+
+	r, err := client.Instances([]string{}, nil)
+
+	if err != nil {
+		return handler.Error(fmt.Sprintf("aws: %s", err.Error()))
+	}
+
+	failedInstances := []string{}
+
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	for _, reservation := range r.Reservations {
+		for _, instance := range reservation.Instances {
+			name := ""
+
+			for _, tag := range instance.Tags {
+				if tag.Key == "Name" {
+					name = tag.Value
+				}
+			}
+
+			if !strings.HasPrefix(name, "core-") {
+				continue
+			}
+
+			log.Println(name)
+
+			wg.Add(1)
+
+			go func() {
+				defer wg.Done()
+
+				timeout := 15 * time.Second
+				client := http.Client{Timeout: timeout}
+				_, err := client.Get(
+					fmt.Sprintf("http://%s:2379/v2/keys", instance.PrivateIpAddress),
+				)
+
+				if err != nil {
+					log.Printf("%s: %s", name, err.Error())
+
+					// Sad, https://github.com/golang/go/issues/4373
+					if strings.HasSuffix(
+						err.Error(),
+						"use of closed network connection",
+					) {
+						mu.Lock()
+						defer mu.Unlock()
+
+						failedInstances = append(failedInstances, instance.InstanceId)
+					}
+				}
+			}()
+		}
+	}
+
+	wg.Wait()
+
+	if len(failedInstances) == 0 {
+		return handler.Ok("Every instances are running")
+	} else {
+		return handler.Error(fmt.Sprintf(
+			"Instances dead: %s",
+			strings.Join(failedInstances, ","),
+		))
+	}
+}
+
+func AWSCheck() check.ExtensionCheckResult {
+	client := buildEc2Client()
 
 	r, err := client.DescribeInstanceStatus(&ec2.DescribeInstanceStatus{}, nil)
 
 	if err != nil {
-		log.Println(err.Error())
+		return handler.Error(fmt.Sprintf("aws: %s", err.Error()))
 	}
 
 	failedInstances := []string{}
@@ -49,7 +127,8 @@ func main() {
 	t := transport.NewRabbitMQTransport(cfg)
 	client := sensu.NewClient(t, cfg)
 
-	check.Store["sensu-aws-client"] = &check.ExtensionCheck{AWSCheck}
+	check.Store["aws-nodes-health-check"] = &check.ExtensionCheck{AWSCheck}
+	check.Store["aws-nodes-etcd-check"] = &check.ExtensionCheck{EtcdGlobalCheck}
 
 	client.Start()
 }
