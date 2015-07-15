25d900606fdd7b536e0a4af022312a784ce91529
Alexis Montagne <alexis.montagne@gmail.com>
2015-07-14 17:23:12 -0700

    Add some memory metrics

--- clients/sensu-elasticsearch-client/check.go
+++ clients/sensu-elasticsearch-client/check.go
@@ -95,6 +95,45 @@ func ClusterSizeCheck() check.ExtensionCheckResult {
 	}
 }
 
+func MemoryMetric() check.ExtensionCheckResult {
+	metric := handler.Metric{}
+
+	c, err := elasticsearchConn()
+
+	if err != nil {
+		return metric.Render()
+	}
+
+	stats, err := c.NodesStats()
+
+	if err != nil {
+		return metric.Render()
+	}
+
+	for _, n := range stats.Nodes {
+		metric.AddPoint(
+			handler.Point{
+				fmt.Sprintf("elasticsearch.%s.heap_size", n.Name),
+				float64(n.JVM.Mem.HeapUsedInBytes / (1024 * 1024)),
+			},
+		)
+		metric.AddPoint(
+			handler.Point{
+				fmt.Sprintf("elasticsearch.%s.swap_size", n.Name),
+				float64(n.OS.Swap.Used / (1024 * 1024)),
+			},
+		)
+		metric.AddPoint(
+			handler.Point{
+				fmt.Sprintf("elasticsearch.%s.mem_size", n.Name),
+				float64(n.OS.Mem.Used / (1024 * 1024)),
+			},
+		)
+	}
+
+	return metric.Render()
+}
+
 func HeapSizeCheck() check.ExtensionCheckResult {
 	warningHeapSize := WARNING_HEAP_SIZE
 	errorHeapSize := ERROR_HEAP_SIZE
