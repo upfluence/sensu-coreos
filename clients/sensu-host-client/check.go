5b2ba009b4e2c5a06c416adbd7a55ce118e3f188
Alexis Montagne <alexis.montagne@gmail.com>
2015-07-21 17:18:23 -0700

    Initial implementation

---
+++ clients/sensu-host-client/check.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/cloudfoundry/gosigar"
+	"github.com/upfluence/sensu-client-go/sensu"
+	"github.com/upfluence/sensu-client-go/sensu/check"
+	"github.com/upfluence/sensu-client-go/sensu/handler"
+	"github.com/upfluence/sensu-client-go/sensu/transport"
+)
+
+const (
+	_          = iota
+	KB float64 = 1 << (10 * iota)
+	MB
+	GB
+	LOAD_AVERAGE_WARNING = 1.0
+	LOAD_AVERAGE_ERROR   = 2.0
+	MEM_ERROR            = 3.9 * GB
+	MEM_WARNING          = 3.7 * GB
+	SWAP_ERROR           = 1.9 * GB
+	SWAP_WARNING         = 1.7 * GB
+	DISK_WARNING         = 130 * GB
+	DISK_ERROR           = 150 * GB
+	DOCKER_VSZ_ERROR     = 3.2 * GB
+	DOCKER_VSZ_WARNING   = 2.5 * GB
+)
+
+type Check struct {
+	Name             string
+	errorThreshold   float64
+	warningThreshold float64
+	fetchValue       func() (float64, error)
+}
+
+func (c *Check) Metric() check.ExtensionCheckResult {
+	metric := &handler.Metric{}
+
+	value := 0.0
+	if v, err := c.fetchValue(); err != nil {
+		return metric.Render()
+	} else {
+		value = v
+	}
+
+	metric.AddPoint(
+		handler.Point{
+			fmt.Sprintf("%s.%s", os.Getenv("SENSU_HOSTNAME"), c.Name),
+			value,
+		},
+	)
+
+	return metric.Render()
+}
+
+func (c *Check) Check() check.ExtensionCheckResult {
+	value := 0.0
+	if v, err := c.fetchValue(); err != nil {
+		return handler.Error(fmt.Sprintf("%s: %s", c.Name, err.Error()))
+	} else {
+		value = v
+	}
+
+	message := fmt.Sprintf("%s: %f", c.Name, value)
+
+	if value > c.errorThreshold {
+		return handler.Error(message)
+	} else if value > c.warningThreshold {
+		return handler.Warning(message)
+	}
+
+	return handler.Ok(message)
+}
+
+var (
+	sgr      = &sigar.ConcreteSigar{}
+	memCheck = &Check{
+		Name:             "mem",
+		errorThreshold:   MEM_ERROR,
+		warningThreshold: MEM_WARNING,
+		fetchValue: func() (float64, error) {
+			v, err := sgr.GetMem()
+
+			if err != nil {
+				return 0.0, err
+			}
+
+			return float64(v.Used), nil
+		},
+	}
+
+	swapCheck = &Check{
+		Name:             "Swap",
+		errorThreshold:   SWAP_ERROR,
+		warningThreshold: SWAP_WARNING,
+		fetchValue: func() (float64, error) {
+			v, err := sgr.GetSwap()
+
+			if err != nil {
+				return 0.0, err
+			}
+
+			return float64(v.Used), nil
+		},
+	}
+
+	loadAverageCheck = &Check{
+		Name:             "load_average",
+		errorThreshold:   LOAD_AVERAGE_ERROR,
+		warningThreshold: LOAD_AVERAGE_WARNING,
+		fetchValue: func() (float64, error) {
+			v, err := sgr.GetLoadAverage()
+
+			if err != nil {
+				return 0.0, err
+			}
+
+			return v.One, nil
+		},
+	}
+
+	diskCheck = &Check{
+		Name:             "disk",
+		errorThreshold:   DISK_ERROR,
+		warningThreshold: DISK_WARNING,
+		fetchValue: func() (float64, error) {
+			v, err := sgr.GetFileSystemUsage("/")
+
+			if err != nil {
+				return 0.0, err
+			}
+
+			return float64(v.Used), nil
+		},
+	}
+
+	cpuCheck = &Check{
+		Name:             "cpu",
+		errorThreshold:   0.0,
+		warningThreshold: 0.0,
+		fetchValue: func() (float64, error) {
+			responseChan, _ := sgr.CollectCpuStats(5 * time.Second)
+
+			v := <-responseChan
+
+			return float64(v.Total()), nil
+		},
+	}
+
+	dockerVSZCheck = &Check{
+		Name:             "cpu",
+		errorThreshold:   DOCKER_VSZ_ERROR,
+		warningThreshold: DOCKER_VSZ_ERROR,
+		fetchValue: func() (float64, error) {
+			f, err := ioutil.ReadFile("/var/run/docker.pid")
+
+			if err != nil {
+				return 0.0, err
+			}
+
+			pid, err := strconv.Atoi(string(f))
+
+			if err != nil {
+				return 0.0, err
+			}
+
+			mem := sigar.ProcMem{}
+
+			if err := mem.Get(pid); err != nil {
+				return 0.0, err
+			}
+
+			return float64(mem.Size), nil
+		},
+	}
+)
+
+func main() {
+	cfg := sensu.NewConfigFromFlagSet(sensu.ExtractFlags())
+
+	t := transport.NewRabbitMQTransport(cfg)
+	client := sensu.NewClient(t, cfg)
+
+	check.Store["host-mem-check"] = &check.ExtensionCheck{memCheck.Check}
+	check.Store["host-disk-check"] = &check.ExtensionCheck{diskCheck.Check}
+	check.Store["host-docker_vsz-check"] = &check.ExtensionCheck{
+		dockerVSZCheck.Check,
+	}
+	check.Store["host-load_average-check"] = &check.ExtensionCheck{
+		loadAverageCheck.Check,
+	}
+	check.Store["host-mem-metric"] = &check.ExtensionCheck{memCheck.Metric}
+	check.Store["host-disk-metric"] = &check.ExtensionCheck{diskCheck.Metric}
+	check.Store["host-cpu-metric"] = &check.ExtensionCheck{diskCheck.Metric}
+	check.Store["host-load_average-metric"] = &check.ExtensionCheck{
+		loadAverageCheck.Metric,
+	}
+	check.Store["host-docker_vsz-metric"] = &check.ExtensionCheck{
+		dockerVSZCheck.Metric,
+	}
+
+	client.Start()
+}
