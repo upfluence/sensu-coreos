6bacdacf737c392d1093afe392bf3cb68e655bae
Alexis Montagne <alexis.montagne@gmail.com>
2015-12-02 13:21:13 -0800

    Also move the client to exe/ directory

--- sensu-client/run.sh
+++ sensu-client/run.sh
@@ -2,4 +2,4 @@
 
 ruby env_to_config.rb
 
-etcdenv -n $SENSU_NAMESPACE -s http://172.17.42.1:4001 /sensu/bin/sensu-client -d /etc/sensu/conf.d -v
+etcdenv -n $SENSU_NAMESPACE -s http://172.17.42.1:4001 /sensu/exe/sensu-client -d /etc/sensu/conf.d -v
