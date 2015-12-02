eb8b9c7d740fd5d548266bd11a6a731dd4b189fe
Alexis Montagne <alexis.montagne@gmail.com>
2015-12-01 20:03:58 -0800

    Move the sensu executable path to /sensu/exe

--- sensu-server/run.sh
+++ sensu-server/run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-confd -verbose -onetime -backend etcd -node 172.17.42.1:4001 -config-file /etc/confd/conf.d/checks.toml
+confd -verbose -onetime -backend etcd -node 172.17.42.1:4001 \
+  -config-file /etc/confd/conf.d/checks.toml
 
-confd -verbose -interval 3 -backend etcd -node 172.17.42.1:4001 -config-file /etc/confd/conf.d/checks.toml > /dev/stdout 2>&1 &
+confd -verbose -interval 3 -backend etcd -node 172.17.42.1:4001 \
+  -config-file /etc/confd/conf.d/checks.toml > /dev/stdout 2>&1 &
 
-etcdenv -n $SENSU_NAMESPACE -s http://172.17.42.1:4001 /sensu/bin/sensu-server -d /etc/sensu/conf.d -v -p /var/run/sensu.pid
+etcdenv -n $SENSU_NAMESPACE -s http://172.17.42.1:4001 /sensu/exe/sensu-server \
+  -d /etc/sensu/conf.d -v -p /var/run/sensu.pid
