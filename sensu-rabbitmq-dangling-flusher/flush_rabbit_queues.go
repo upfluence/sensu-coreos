c5c025a2931c8a5ff972e8abf573ccbe82f3de7d
Philippe Ndiaye <phndiaye@gmail.com>
2015-12-30 14:58:36 +0100

    Flush the dangling queues and connections related to Sensu's rabbitmq

---
+++ sensu-rabbitmq-dangling-flusher/flush_rabbit_queues.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/michaelklishin/rabbit-hole"
+	"log"
+	"os"
+)
+
+const DEFAULT_RABBITMQ_ADMIN_URL = "http://127.0.0.1:15672"
+
+var rabbitmqAdminURL = os.Getenv("RABBITMQ_ADMIN_URL")
+
+func main() {
+	if rabbitmqAdminURL != "" {
+		rabbitmqAdminURL = rabbitmqAdminURL
+	} else {
+		rabbitmqAdminURL = DEFAULT_RABBITMQ_ADMIN_URL
+	}
+
+	rmqc, err := rabbithole.NewClient(rabbitmqAdminURL, "guest", "guest")
+
+	if err != nil {
+		panic(err)
+	}
+
+	queues, _ := rmqc.ListQueues()
+	for _, queue := range queues {
+		if queue.Consumers == 0 {
+			log.Println("Deleting %s...", queue.Name)
+			_, err := rmqc.DeleteQueue("/", queue.Name)
+			if err != nil {
+				log.Printf(err.Error())
+			}
+		}
+	}
+
+	connections, _ := rmqc.ListConnections()
+	for _, conn := range connections {
+		if conn.Channels == 0 {
+			log.Println("Closing connection %s...", conn.Name)
+			_, err := rmqc.CloseConnection(conn.Name)
+			if err != nil {
+				log.Printf(err.Error())
+			}
+		}
+	}
+}
