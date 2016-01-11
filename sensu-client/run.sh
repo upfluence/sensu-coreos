fafba1287831a54b32d0480cade15ebe12f42fa7
Alexis Montagne <alexis.montagne@gmail.com>
2016-01-10 22:49:37 -0800

    Move back to sensu 0.20.x

--- sensu-client/run.sh
+++ sensu-client/run.sh
@@ -2,4 +2,4 @@
 
 ruby env_to_config.rb
 
-exec etcdenv -n $SENSU_NAMESPACE -s http://172.17.42.1:4001 /sensu/exe/sensu-client -d /etc/sensu/conf.d -v
+exec etcdenv -n $SENSU_NAMESPACE -s http://172.17.42.1:4001 /sensu/bin/sensu-client -d /etc/sensu/conf.d -v
--- sensu-server/run.sh
+++ sensu-server/run.sh
@@ -6,5 +6,5 @@ confd -verbose -onetime -backend etcd -node 172.17.42.1:4001 \
 confd -verbose -interval 3 -backend etcd -node 172.17.42.1:4001 \
   -config-file /etc/confd/conf.d/checks.toml > /dev/stdout 2>&1 &
 
-exec etcdenv -n $SENSU_NAMESPACE -s http://172.17.42.1:4001 /sensu/exe/sensu-server \
+exec etcdenv -n $SENSU_NAMESPACE -s http://172.17.42.1:4001 /sensu/bin/sensu-server \
   -d /etc/sensu/conf.d -v -p /var/run/sensu.pid
