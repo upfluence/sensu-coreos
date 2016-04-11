178c984562fc52fd9f19d9f338801a406cb9b8a3
Alexis Montagne <alexis.montagne@gmail.com>
2016-04-10 23:51:04 -0700

    sensu-thrift-client: Add check to detect too old (stale) services

--- clients/sensu-thrift-client/check.go
+++ clients/sensu-thrift-client/check.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -22,7 +21,7 @@ import (
 )
 
 const (
-	defaultTimeout = 15
+	defaultTimeout = 20 * time.Second
 	defaultAMQPURL = "amqp://guest:guest@localhost:5672/%2f"
 )
 
@@ -30,78 +29,97 @@ type ThriftServiceConfiguration struct {
 	Transport       string            `json:"transport"`
 	Protocol        string            `json:"protocol"`
 	TransportConfig map[string]string `json:"transport_config"`
+	LifeTime        int               `json:"life_time,omitempty"`
 }
 
-func checkService(
+func buildBaseClient(
 	config ThriftServiceConfiguration,
-	name string,
 	rmqConn *amqp.Connection,
 	rmqChannel *amqp.Channel,
-) bool {
-	var trans thrift.TTransport
-	amqpURL := defaultAMQPURL
-	timeout := defaultTimeout
-
-	if os.Getenv("TIMEOUT") != "" {
-		if t, err := strconv.Atoi(os.Getenv("TIMEOUT")); err == nil {
-			timeout = t
-		}
-	}
+) (*base_service.BaseServiceClient, thrift.TTransport, error) {
+	var (
+		trans    thrift.TTransport
+		protocol thrift.TProtocolFactory
+	)
 
 	if config.Transport == "http" {
-		log.Printf(
-			"%s: type:http url:%s",
-			name,
-			config.TransportConfig["url"],
-		)
-
 		trans, _ = thrift.NewTHttpPostClient(config.TransportConfig["url"])
 	} else {
-		log.Printf(
-			"%s: type:amqp exchange:%s routing_key:%s url:%s",
-			name,
-			config.TransportConfig["exchange"],
-			config.TransportConfig["routing"],
-			amqpURL,
-		)
-
 		trans, _ = amqp_thrift.NewTAMQPClientFromConn(
 			rmqConn,
 			rmqChannel,
 			config.TransportConfig["exchange"],
 			config.TransportConfig["routing"],
 			"sensu-thrift-client",
+			0,
 		)
 	}
 
 	if err := trans.Open(); err != nil {
-		log.Printf("%s open error:%s", name, err.Error())
-		return false
+		return nil, nil, err
 	}
-	defer trans.Close()
 
-	var protocol thrift.TProtocolFactory
 	protocol = thrift.NewTBinaryProtocolFactoryDefault()
 
 	if config.Protocol == "json" {
 		protocol = thrift.NewTJSONProtocolFactory()
 	}
 
-	client := base_service.NewBaseServiceClientFactory(
+	return base_service.NewBaseServiceClientFactory(
 		trans,
 		protocol,
-	)
+	), trans, nil
+}
 
+func checkDurationService(
+	name string,
+	client *base_service.BaseServiceClient,
+	timeout int,
+) bool {
+	c := make(chan int64, 1)
+
+	go func() {
+		s, err := client.AliveSince()
+		if err != nil {
+			log.Printf("duration: %s: error:%s", name, err.Error())
+
+			s, err = client.AliveSince()
+			if err != nil {
+				log.Printf("duration: %s: 2nd error:%s", name, err.Error())
+			}
+		}
+
+		c <- s
+	}()
+
+	select {
+	case r := <-c:
+		log.Printf("%s alive since %+v", name, time.Unix(r, 0))
+
+		if time.Now().Unix()-r > int64(timeout) {
+			return false
+		} else {
+			return true
+		}
+	case <-time.After(defaultTimeout):
+		return true
+	}
+}
+
+func checkStatusService(
+	name string,
+	client *base_service.BaseServiceClient,
+) bool {
 	c := make(chan base_service.Status, 1)
 
 	go func() {
 		s, err := client.GetStatus()
 		if err != nil {
-			log.Printf("%s: error:%s", name, err.Error())
+			log.Printf("status: %s: error:%s", name, err.Error())
 
 			s, err = client.GetStatus()
 			if err != nil {
-				log.Printf("%s: 2nd error:%s", name, err.Error())
+				log.Printf("status: %s: 2nd error:%s", name, err.Error())
 			}
 		}
 
@@ -115,13 +133,21 @@ func checkService(
 		} else {
 			return false
 		}
-	case <-time.After(time.Duration(timeout) * time.Second):
+	case <-time.After(defaultTimeout):
 		return false
 	}
 }
 
-func ThriftCheck() check.ExtensionCheckResult {
-	machines := []string{}
+type serviceConfig struct {
+	configuration *ThriftServiceConfiguration
+	name          string
+}
+
+func serviceConfigurations() ([]serviceConfig, error) {
+	var (
+		machines = []string{}
+		results  = []serviceConfig{}
+	)
 
 	if os.Getenv("ETCD_URL") == "" {
 		machines = append(machines, "http://172.17.42.1:2379")
@@ -134,48 +160,84 @@ func ThriftCheck() check.ExtensionCheckResult {
 	resp, err := etcdClient.Get("/sensu/services", false, true)
 
 	if err != nil {
-		return handler.Error(fmt.Sprintf("etcd: %s", err.Error()))
+		return results, fmt.Errorf("etcd: %s", err.Error())
 	}
-
-	rmqConn, err := amqp.Dial(os.Getenv("RABBITMQ_URL"))
-	if err != nil {
-		return handler.Error(fmt.Sprintf("rabbitmq: %s", err.Error()))
-	}
-
-	defer rmqConn.Close()
-
-	rmqChannel, err := rmqConn.Channel()
-	if err != nil {
-		return handler.Error(fmt.Sprintf("rabbitmq: %s", err.Error()))
-	}
-
-	failedServices := []string{}
-	var wg sync.WaitGroup
-	var mu sync.Mutex
 
 	for _, node := range resp.Node.Nodes {
 		var config ThriftServiceConfiguration
 
+		parts := strings.Split(node.Key, "/")
+		service := parts[len(parts)-1]
 		err := json.Unmarshal([]byte(node.Value), &config)
 
 		if err != nil {
-			return handler.Error(fmt.Sprintf("json: %s", err.Error()))
+			return results, fmt.Errorf("json: %s: %s", service, err.Error())
+		} else {
+			results = append(
+				results,
+				serviceConfig{&config, service},
+			)
 		}
+	}
 
+	return results, nil
+}
+
+func buildRabbitMQConnection() (*amqp.Connection, *amqp.Channel, error) {
+	rmqConn, err := amqp.Dial(os.Getenv("RABBITMQ_URL"))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	rmqChannel, err := rmqConn.Channel()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return rmqConn, rmqChannel, nil
+}
+
+func statusCheck() check.ExtensionCheckResult {
+	var (
+		failedServices = []string{}
+
+		wg sync.WaitGroup
+		mu sync.Mutex
+	)
+
+	rmqConn, rmqChannel, err := buildRabbitMQConnection()
+	if err != nil {
+		return handler.Error(err.Error())
+	}
+
+	defer rmqConn.Close()
+
+	configs, err := serviceConfigurations()
+
+	if err != nil {
+		return handler.Error(fmt.Sprintf("rabbitmq: %s", err.Error()))
+	}
+
+	for _, config := range configs {
 		wg.Add(1)
 
-		go func(config ThriftServiceConfiguration, node *etcd.Node) {
+		go func(config *ThriftServiceConfiguration, name string) {
 			defer wg.Done()
+			baseClient, trans, err := buildBaseClient(*config, rmqConn, rmqChannel)
 
-			parts := strings.Split(node.Key, "/")
-			service := parts[len(parts)-1]
+			if err != nil {
+				log.Printf("%s: %s", name, err.Error())
+				return
+			}
 
-			if !checkService(config, service, rmqConn, rmqChannel) {
+			defer trans.Close()
+
+			if !checkStatusService(name, baseClient) {
 				mu.Lock()
 				defer mu.Unlock()
-				failedServices = append(failedServices, service)
+				failedServices = append(failedServices, name)
 			}
-		}(config, node)
+		}(config.configuration, config.name)
 	}
 
 	wg.Wait()
@@ -190,13 +252,73 @@ func ThriftCheck() check.ExtensionCheckResult {
 	}
 }
 
+func durationCheck() check.ExtensionCheckResult {
+	var (
+		failedServices = []string{}
+
+		wg sync.WaitGroup
+		mu sync.Mutex
+	)
+
+	rmqConn, rmqChannel, err := buildRabbitMQConnection()
+	if err != nil {
+		return handler.Error(err.Error())
+	}
+
+	defer rmqConn.Close()
+
+	configs, err := serviceConfigurations()
+
+	if err != nil {
+		return handler.Error(fmt.Sprintf("rabbitmq: %s", err.Error()))
+	}
+
+	for _, config := range configs {
+		if t := config.configuration.LifeTime; t != 0 {
+			wg.Add(1)
+
+			go func(config *ThriftServiceConfiguration, name string) {
+				defer wg.Done()
+				baseClient, trans, err := buildBaseClient(*config, rmqConn, rmqChannel)
+
+				if err != nil {
+					log.Printf("%s: %s", name, err.Error())
+					return
+				}
+
+				defer trans.Close()
+
+				if !checkDurationService(name, baseClient, t) {
+					mu.Lock()
+					defer mu.Unlock()
+					failedServices = append(failedServices, name)
+				}
+			}(config.configuration, config.name)
+		}
+	}
+
+	wg.Wait()
+
+	if len(failedServices) == 0 {
+		return handler.Ok("Every thrift services are ok")
+	} else {
+		return handler.Error(
+			fmt.Sprintf(
+				"Thrift services too old: %s",
+				strings.Join(failedServices, ","),
+			),
+		)
+	}
+}
+
 func main() {
 	cfg := sensu.NewConfigFromFlagSet(sensu.ExtractFlags())
 
 	t := transport.NewRabbitMQTransport(cfg)
 	client := sensu.NewClient(t, cfg)
 
-	check.Store["sensu-thrift-client"] = &check.ExtensionCheck{ThriftCheck}
+	check.Store["thrift-status-check"] = &check.ExtensionCheck{statusCheck}
+	check.Store["thrift-duration-check"] = &check.ExtensionCheck{durationCheck}
 
 	client.Start()
 }
