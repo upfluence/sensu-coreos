850407e897a792c0e310d661ce152ece513c5930
Alexis Montagne <alexis.montagne@gmail.com>
2016-07-15 17:57:12 -0400

    FIx up the API run.sh file

--- sensu-api/run.sh
+++ sensu-api/run.sh
@@ -6,5 +6,5 @@ confd -verbose -onetime -backend etcd -node 172.17.42.1:4001 \
 confd -verbose -interval 3 -backend etcd -node 172.17.42.1:4001 \
   -config-file /etc/confd/conf.d/checks.toml > /dev/stdout 2>&1 &
 
-exec etcdenv -n $sensu_namespace -s http://172.17.42.1:4001 /sensu/bin/sensu-api \
-  -d /etc/sensu/conf.d  -l ${log_level:-"warn"} -p /var/run/sensu.pid
+exec etcdenv -n $SENSU_NAMESPACE -s http://172.17.42.1:4001 /sensu/bin/sensu-api \
+  -d /etc/sensu/conf.d  -L ${LOG_LEVEL:-"WARN"} -p /var/run/sensu.pid
